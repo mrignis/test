5de250c2c18866e2d8a3194ffc355d2d26a85251
PYLYP CHELBARAKH <143128807+mrignis@users.noreply.github.com>
2024-01-10 00:12:53 -0500

    wss

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -51,7 +51,7 @@ const images = [
   },
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',т
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
     description: 'Nature Landscape',
